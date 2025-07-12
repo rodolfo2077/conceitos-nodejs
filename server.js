1dc3fb31b47a3a7dc56fa115e0bfdaf14d112144
rodolfo2077 <rodolfocorreia.nascimento@outlook.com.br>
2025-07-12 03:49:48 -0300

    Adicionado um index.html e Implementando o Fetch para consumir API.

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { PrismaClient } from "./generated/prisma/index.js";
+import cors from "cors";
 
 const app = express();
 const prisma = new PrismaClient();
+app.use(cors()); // Permite qualquer origem
 
 const port = 3000;
 app.use(express.json());
@@ -10,6 +12,23 @@ app.use(express.json());
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
   console.log(`Acesse pelo http://localhost:${port}`);
+});
+
+// Buscar um usuário pelo ID
+app.get("/users/:id", async (req, res) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: req.params.id },
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "Usuário não encontrado" });
+    }
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 // Criar rota POST para criar usuario no banco de dados mongo via framework prisma
@@ -29,15 +48,15 @@ app.post("/users", async (req, res) => {
   }
 });
 
-// Cria rota GET para listar todos usuarios da tabela user do mongodb
-app.get("/users", async (req, res) => {
-  try {
-    const users = await prisma.user.findMany();
-    res.status(200).json(users);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+// // Cria rota GET para listar todos usuarios da tabela user do mongodb
+// app.get("/users", async (req, res) => {
+//   try {
+//     const users = await prisma.user.findMany();
+//     res.status(200).json(users);
+//   } catch (err) {
+//     res.status(500).json({ error: err.message });
+//   }
+// });
 
 // Criar rota PUT para editar , atualizar o usuario pelo id
 app.put("/users/:id", async (req, res) => {
