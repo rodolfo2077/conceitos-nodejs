370811c90f2f497b49faba5f7cac6d410764bc0f
rodolfo2077 <rodolfocorreia.nascimento@outlook.com.br>
2025-07-12 00:17:11 -0300

    Implementado boas práticas de coleta de erros com o try e catch.

--- server.js
+++ server.js
@@ -14,46 +14,63 @@ app.listen(port, () => {
 
 // Criar rota POST para criar usuario no banco de dados mongo via framework prisma
 app.post("/users", async (req, res) => {
-  const user = await prisma.user.create({
-    data: {
-      email: req.body.email,
-      name: req.body.name,
-      age: req.body.age,
-    },
-  });
-  res.status(201).json(user);
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: req.body.email,
+        name: req.body.name,
+        age: req.body.age,
+      },
+    });
+
+    res.status(201).json(`Usuário criado com succeso: ${user}`);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 // Cria rota GET para listar todos usuarios da tabela user do mongodb
 app.get("/users", async (req, res) => {
-  const users = await prisma.user.findMany();
-  res.status(200).json(users);
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 // Criar rota PUT para editar , atualizar o usuario pelo id
 app.put("/users/:id", async (req, res) => {
-  const users = await prisma.user.update({
-    where: {
-      id: req.params.id,
-    },
-    data: {
-      email: req.body.email,
-      name: req.body.name,
-      age: req.body.age,
-    },
-  });
+  try {
+    const users = await prisma.user.update({
+      where: {
+        id: req.params.id,
+      },
+      data: {
+        email: req.body.email,
+        name: req.body.name,
+        age: req.body.age,
+      },
+    });
 
-  res.status(200).json(users);
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 // Criar rota DELETE
 app.delete("/users/:id", async (req, res) => {
-  await prisma.user.delete({
-    where: {
-      id: req.params.id,
-    },
-  });
-  res.status(200).json({ message: "Usuário deletado com sucesso!" });
+  try {
+    await prisma.user.delete({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json({ message: "Usuário deletado com sucesso!" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 // CRUD Finalizado
